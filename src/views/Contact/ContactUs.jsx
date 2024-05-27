4fb4b117b967c2bf8a020adb2c00eddf9c87d150
Mark5-sys <mark.marteus5@gmail.com>
2024-05-27 16:01:30 +0200

    contact page

--- src/views/Contact/ContactUs.jsx
+++ src/views/Contact/ContactUs.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <Box p={4}>
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        textAlign="center"
+        color="green.500"
+      >
+        How can we help?
+      </Text>
+      <Tabs vundefinednt="soft-rounded" colorScheme="green" mt={4}>
+        <TabList justifyContent="center">
+          <Tab>Medical Insurance</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Box bg="gray.100" p={4} borderRadius="md">
+              <Text fontSize="xl" fontWeight="bold" color="green.500">
+                Medical Insurance
+              </Text>
+              <Text mt={2}>
+                <strong>Sales:</strong> <br />
+                0861 029 892 <br />
+                <a href="mailto:customerengagement@dischemhealth.co.za">
+                  customerengagement@dischemhealth.co.za
+                </a>
+              </Text>
+              <Text mt={2}>
+                <strong>Customer Support and General queries:</strong> <br />
+                0861 029 892 <br />
+                <a href="mailto:dischemhealth@kaelo.co.za">
+                  dischemhealth@kaelo.co.za
+                </a>
+              </Text>
+              <Text mt={2}>
+                <strong>Family, Legal or Financial Counselling:</strong> <br />
+                0861 635 766 <br />
+                (powered by Kaelo AskNelson)
+              </Text>
+              <Text mt={2}>
+                <strong>Emergency – Netcare911:</strong> <br />
+                082 911
+              </Text>
+            </Box>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
